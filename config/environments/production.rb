db9ef05178512fc0663b5e272951fbd00534dfa4
= <nicolas.papon@webflows.fr>
2010-10-06 15:32:42 +0200

    io

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,17 @@ Morph::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'morph.webflows.fr' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.smtp_settings = {
+    :address   => 'localhost',
+    :port      => 25
+  }
+
+  config.middleware.use "::ExceptionNotifier",
+     :email_prefix => "[MORPH] ",
+     :sender_address => %{"Rails" <notifier@w3bflows.com>},
+     :exception_recipients => %w{nicolas@w3bflows.com}
+
 end
